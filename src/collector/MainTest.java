f78d28c30fc8a4f54009b92e65cb95102359d406
DanielGunna <danielgunna1408@gmail.com>
2017-02-14 16:41:27 -0200

    Initial commit

---
+++ src/collector/MainTest.java
@@ -0,0 +1,16 @@
+package collector;
+
+import org.jsoup.nodes.Document;
+
+public class MainTest {
+	
+	public static void main(String[] args){
+		try{
+			Document doc  =  RequestHandler.getInstance().getDataFromUrl("https://www.youtube.com/watch?v=F-XhH1FTKm0");
+			System.out.println(doc.toString());
+		}catch (Exception e ){
+			e.printStackTrace();
+		}
+	}
+
+}
---
+++ src/collector/RequestHandler.java
@@ -0,0 +1,24 @@
+package collector;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+
+public class RequestHandler {
+	static RequestHandler instance;
+	
+	
+	public static RequestHandler getInstance(){
+		if(instance == null ){
+			instance = new RequestHandler();
+		}
+		return instance;
+	}
+	
+	public Document getDataFromUrl(String url )throws Exception{
+		Document document =  Jsoup.connect(url).get();
+		return document;
+	}
+	
+	
+	
+}
