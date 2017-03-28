006e3de101ded0ecfdfe738c5fab98ccee115121
Marcelo Rodrigues Dos Santos Junior <marrsantosjunior@gmail.com>
2017-03-27 21:20:32 -0300

    - Parser;

--- src/collector/MainTest.java
+++ src/collector/MainTest.java
@@ -1,17 +1,34 @@
 package collector;
 
+import java.io.IOException;
+import java.net.URL;
+
+import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 
 public class MainTest {
 	
-	public static void main(String[] args){
-		//try{
-		//	Document doc  =  RequestHandler.getInstance().getDataFromUrl("https://www.youtube.com/watch?v=F-XhH1FTKm0");
-	//		System.out.println(doc.toString());
-	//	}catch (Exception e ){
-	//		e.printStackTrace();
-		//}
-		new DNSResolver().resolveAddress("www.google.com");
-	}
+	 public static void main(String[] args) throws IOException {
+	        //Validate.isTrue(args.length == 1, "usage: supply url to fetch");
+	        String url = "http://globo.com";
+	        print("Fetching %s...", url);
+
+	        Document doc = Jsoup.parse(new URL(url).openStream(), "UTF-8", url);
+	        Elements links = doc.select("a[href]");
+
+	        print("\nLinks: (%d)", links.size());
+	        for (Element link : links) {
+	        	
+	            print("%s", link.attr("abs:href"));
+
+	        }
+	    }
+
+	    private static void print(String msg, Object... args) {
+	        System.out.println(String.format(msg, args));
+	    }
+
 
 }
