3602959eeb1715c6ca1378b717afe3a06808f435
DanielGunna <danielgunna1408@gmail.com>
2017-04-09 23:15:32 -0300

    IMplementando cache DnsResolver

--- src/collector/DnsResolver.java
+++ src/collector/DnsResolver.java
@@ -1,44 +1,62 @@
 package collector;
 
-import org.jsoup.nodes.Document;
-import org.jsoup.nodes.Element;
-import org.jsoup.nodes.Node;
-import org.jsoup.*;
-import org.jsoup.Connection.Response;
+ 
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.Serializable;
 import java.net.InetAddress;
 import java.net.URI;
 import java.net.URL;
 
 import sun.net.spi.nameservice.dns.*;
 import java.rmi.UnknownHostException;
-import java.util.Arrays;
+import java.util.ArrayList;
+ 
 import java.util.HashMap;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-
-import javax.management.InstanceAlreadyExistsException;
+import java.util.List;
+ 
 
 public class DnsResolver{
 	
-	private static DnsResolver instance;
-	private static final String TAG ="DNSResolver: "; 
-	private HashMap<String,InetAddress> addressTable;
-	private RequestHandler requestHandler;
-	private static final String userAgent = "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) "
-			+ "Chrome/56.0.2924.87 Safari/537.36";
 	
+	private static final String TAG ="DNSResolver: "; 
+	private static final int MAX_CACHED_ADDRESS = 5000;
+	private static final String userAgent = "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) "
+											+ "Chrome/56.0.2924.87 Safari/537.36";
+	private static DnsResolver instance;
+	private HashMap<String,InetAddress> addressTable;
+	private HashMap<String,CacheMetadata> addressCache;
+	private int totalCachedAddress = 0 ;
+	
+
 	private DnsResolver() {
 		addressTable = new HashMap<>();
-		requestHandler = new RequestHandler();
+		addressCache = new HashMap<>();
 	}
 	
 	private void indexAddress(InetAddress address, URI hostName){
 		addressTable.put(hostName.toString(), address);
 		logAddressInfo(address);
-		getRootRobots(address);
+		cacheAddress(new CacheMetadata(address, hostName,getParsedRootRobots(address)));
 	}
 	
+	private void cacheAddress(CacheMetadata data) {
+		if(totalCachedAddress <= MAX_CACHED_ADDRESS){
+			cacheOnMemory(data);
+		}else{
+			cacheOnDisk(data);
+		}
+	}
+		
+	private void cacheOnDisk(CacheMetadata data) {
+	}
+
+	private void cacheOnMemory(CacheMetadata data) {
+		addressCache.put(data.getUrl().getHost(), data); 
+	}
+
 	public static DnsResolver getInstance(){
 		if(instance == null ){
 			instance = new DnsResolver();
@@ -65,7 +83,7 @@ public class DnsResolver{
 
 	private void resolveWithDns(URI hostName){
 		try{
-			DNSNameService service = new DNSNameService();
+			DNSNameService service = new DNSNameService(); 
 			InetAddress[] ipAddress = service.lookupAllHostAddr(hostName.toString());
 			indexAddress(ipAddress[1],hostName);
 		}catch (UnknownHostException e){
@@ -79,28 +97,71 @@ public class DnsResolver{
 		System.out.println(TAG+"Host Canonicalname:  "+ address.getCanonicalHostName());
 	}
 	
-	private void getRootRobots(InetAddress address){
-		try {
-			byte[] doc = Jsoup.connect("http://"+address.getHostAddress()+"/robots.txt")
-					.userAgent(userAgent)
-					.execute()
-					.bodyAsBytes();
-			parseRobotsContent(Arrays.toString(doc));
-		} catch (Exception e) {
-			e.printStackTrace();
-		}		
-	}
-
-	private void parseRobotsContent(String text) {
-		String regex = "Disallow:";
-	    Pattern pattern = Pattern.compile(regex,Pattern.DOTALL);
-	    Matcher matcher = pattern.matcher(text);
-	    System.out.println(text);
+	private ArrayList<String> getRobotsTxt(InetAddress address){
+		ArrayList<String> lines = new ArrayList<>();
+		try{
+			URL url = new URL("http://"+address.getHostAddress()+"/robots.txt");
+			BufferedReader in = new BufferedReader(new InputStreamReader(url.openStream()));
+			String line = null;
+	        while((line = in.readLine()) != null) {
+	            lines.add(line);
+	        }
+	    } catch (IOException e) {
+	        e.printStackTrace();
+	    }
+		return  lines;
 	}
 	
- 
-	 
+	private List<String> getParsedRootRobots(InetAddress address){
+		return parseRobotsContent(getRobotsTxt(address));		
+	}
 
-
+	private List<String> parseRobotsContent(ArrayList<String> text) {
+		ArrayList<String> disallowed= new ArrayList<>();
+		for(String  line : text ){
+			if(line.contains("Disallow")){
+				disallowed.add(line.split(":")[1].trim());
+			}
+		}
+		System.out.print(disallowed.toString());
+	    return disallowed;
+	}
+	
+	private class CacheMetadata implements Serializable{
+		private InetAddress address;
+		private URI url;
+		private List<String> disalowedAddresses;
+		private int timesUsed;
+		private long lastUse;
+		
+		public InetAddress getAddress() {
+			return address;
+		}
+		public void setAddress(InetAddress address) {
+			this.address = address;
+		}
+		public URI getUrl() {
+			return url;
+		}
+		public void setUrl(URI url) {
+			this.url = url;
+		}
+		public List<String> getDisalowedAddresses() {
+			return disalowedAddresses;
+		}
+		public void setDisalowedAddresses(List<String> disalowedAddresses) {
+			this.disalowedAddresses = disalowedAddresses;
+		}
+		@Override
+		public String toString() {
+			return "Metadata [address=" + address + ", url=" + url + ", disalowedAddresses=" + disalowedAddresses.toString() + "]";
+		}
+		public CacheMetadata(InetAddress address, URI url, List<String> disalowedAddresses) {
+			this.address = address;
+			this.url = url;
+			this.disalowedAddresses = disalowedAddresses;
+		}
+			
+	}
 }
 
